54671a5aae92f1771c519851841b2a2c78057771
oussema <oussamabenhmida24@gmail.com>
2024-11-29 14:58:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Fonction pour envoyer le formulaire de contact (juste une simulation pour l'exemple)
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci pour votre message, je vous répondrai bientôt.');
+});
